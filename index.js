6b2c4837e302ac73de0372bf2fe13b6f6de5c94e
Grant Klimaytys <grantmk@users.noreply.github.com>
2024-01-26 13:13:05 +0200

    in

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const fs = require("fs");
+const fsp = require('fs/promises')
 
 app.get('/', function (req, res) {
   res.send('Hello World')
@@ -8,12 +10,34 @@ app.get('/', function (req, res) {
 app.listen(3000)
 console.log("Server running on port 3000")
 
-function writeBasicFile () {
+//START section - basic writes
+const textToWrite = "Grant iAmDev"
+
+function writeBasicFileCallback () { //callback version, reference ONLY
   console.log("writeBasicFile called")
-  
+  fs.writeFile("person-callback.txt", textToWrite, "utf8", (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("writeBasicFile - file saved");
+  });
 }
 
-function test () {
-  writeBasicFile()
+async function writeBasicFilePromise() { //async promise version
+  try {
+    await fsp.writeFile("person-promise.txt", textToWrite, "utf8")
+    return true
+  } catch (err) {
+    console.error('Error occurred while writing file:', err)
+    return false
+  }
+}
+//END section - basic writes
+
+async function test () {
+  console.log("START test function call")
+  const success = await writeBasicFilePromise()
+  console.log("File write successful? : " + success)
+  console.log("END test function call")
 }
 test()
