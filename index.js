0a9f1302d8ed7bab554575e9fa5164f56b64d57a
Grant Klimaytys <grantmk@users.noreply.github.com>
2024-02-09 12:40:22 +0000

    in

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ async function test() {
   console.log("START test function call")
 
   //CHANGE THIS LINE then restart server
-  const success = await streamTextToFile("Example text")
+  const success = await streamTextToFileHandleFileClose("Example text")
 
   console.log("File write successful? : " + success)
   console.log("END test function call")
@@ -55,15 +55,46 @@ async function writeBasicFilePromise() { //async promise version
 /* ************************ */
 //START section - stream writes
 async function streamTextToFile(text) {
+  //NOTE: This function combines createWriteStream AND writes to file.
+  // in reality you should separate these 2, only creating the stream once!
   try {
     let writer = await fs.createWriteStream("person-stream.txt", {
       encoding: "utf8",
       highWaterMark: 16384,
-      flags: {
-        
-      }
+      flags: "a" //open and append (don't overwrite file). Omitting this will overwrite whole file.
     })
     await writer.write(text)
+    await writer.write("\r") //create a new line
+    return true
+  } catch (err) {
+    console.error('Error occurred while writing file:', err)
+    return false
+  }
+}
+
+async function streamTextToFileHandleFileClose(text) {
+  // Open a file, turn off auto close, implement a file close function ourselves
+  // and write to log on successful close
+  try {
+    let writer = await fs.createWriteStream("person-stream.txt", {
+      encoding: "utf8",
+      highWaterMark: 16384,
+      autoClose: false,
+      emitClose: true,
+      flags: "a"
+    })
+
+    //add a listener with options such as open, close, ready etc
+    writer.addListener("open", (fd) => {
+      console.log("File opened: ")
+      console.log(fd)
+    })
+
+    await writer.write(text)
+    await writer.write("\r") //create a new line
+    writer.close(() => {
+      console.log("File closed!")
+    })
     return true
   } catch (err) {
     console.error('Error occurred while writing file:', err)
